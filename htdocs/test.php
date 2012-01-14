ea668eb41b2a4bf85edea6299eeb0696d37d0917
Nisjaat Sheik Joesoef <nisjaat@live.nl>
2012-01-14 03:08:21 +0100

    test2

--- htdocs/test.php
+++ htdocs/test.php
@@ -7,17 +7,17 @@ $email = "";
 
 
 // to display errors
-$error="";
+$error= "";
 
 $done=false;
 
 if (isset($_POST["name"]) && isset($_POST["email"])){
     if($_POST["name"]==""){
-    $error = "empty name";
+    $error = "empty name <br/>";
     }
 
     if($_POST["email"]==""){
-    $error =$error . "empty mail";
+    $error = $error . " empty mail <br/>";
     }
 
     $name = $_POST["name"];
@@ -40,10 +40,10 @@ if (isset($_POST["name"]) && isset($_POST["email"])){
 <?php if (!$done){ ?>
     <div>
     <legend><h4>Enter your information in the fields below</h4></legend>
-  
+    <p class="error" style="color:red;"><?php echo $error;?></p>
     <form name="info" method="post" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
-    <strong>Name:</strong> <input type="text" name="name" id="name" value="  <?php echo $name; ?>" /><?php echo $error ;?> <br/>
-    <strong>Email:</strong> <input type="text" name="email" id="email" value=" <?php echo $email ?>" /><?php echo $error; ?> <br/>
+    <strong>Name:</strong> <input type="text" name="name" id="name" value="<?php echo $name; ?>" /><br/>
+    <strong>Email:</strong> <input type="text" name="email" id="email" value="<?php echo $email; ?>" /><br/>
     <br/>
     <br/>
     <input type="submit" name="submit" value="Submit my Information" />
@@ -56,3 +56,4 @@ if (isset($_POST["name"]) && isset($_POST["email"])){
 
 </body>
 </html>
+
