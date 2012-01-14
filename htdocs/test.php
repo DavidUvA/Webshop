0843cad0edc2f6c5f83cdb8f1eb5113c497313ae
Nisjaat Sheik Joesoef <nisjaat@live.nl>
2012-01-14 03:01:26 +0100

    test2

--- htdocs/test.php
+++ htdocs/test.php
@@ -25,7 +25,7 @@ if (isset($_POST["name"]) && isset($_POST["email"])){
     $email = $_POST["email"];
 
 
-    if ($erroremail=="" && $errorname==""){
+    if (($erroremail=="") && ($errorname=="")){
         $done=true;
     }
 }
