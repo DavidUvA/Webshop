bcb452c36235a5684be337bc7ec650c7071d6572
Artiom13 <artiom_emelianov@hotmail.com>
2012-01-23 16:18:15 +0100

    trollolololol

--- htdocs/login.php
+++ htdocs/login.php
@@ -22,9 +22,7 @@ $mypassword = mysql_real_escape_string($mypassword);
 $sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
 $result=mysql_query($sql);
 
-// Mysql_num_row is counting table row
-$count=mysql_num_rows($result);
-// If result matched $myusername and $mypassword, table row must be 1 row
+
 
 if($count==1){
 // Register $myusername, $mypassword and redirect to file "login_success.php"
